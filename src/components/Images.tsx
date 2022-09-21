be5b9f9b594cad1f1a9d000079672f3445c2cbdd
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-09-21 15:10:55 +0200

    feat(lazy-load): add lazy-load to images

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,48 +1,20 @@
-import React from 'react'
-import Image from '../subComponents/Image'
-
-
-
-
-
-
+import React from "react";
+import Image from "../subComponents/Image";
 
 interface ImageProps {
   images: any;
-
-  
-  
 }
 
-
-const Images = ( {images}: ImageProps,  ) => {
-
-
-
- return (
-   
-       
-<section className=' flex flex-col items-center justify-center'>
-
-    <div className=' grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 my-6 gap-2 mx-3 '>
-      
-
-
-
-{images.map((image: any, i: number) => {
-return (  
-
-<Image  image={image} key={image.id} /> 
-)
-  
-})
-}
-
+const Images = ({ images }: ImageProps) => {
+  return (
+    <section className=" flex flex-col items-center justify-center">
+      <div className=" grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 my-6 gap-2 mx-3 ">
+        {images.map((image: any, i: number) => (
+          <Image image={image} key={image.id} />
+        ))}
       </div>
     </section>
+  );
+};
 
-    
-  )
-}
-
-export default Images
+export default Images;
