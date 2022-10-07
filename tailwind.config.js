445b433b41904fdb693157fff989a13690726db7
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-10-07 11:39:42 +0200

    ci: add animation to error message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slide: {
+          from: { transform: "translateY(-100%)" },
+
+          to: { transform: "translateY(0%)" },
+        },
+      },
+      slideBack: {
+        from: { transform: "translateY(0%)" },
+
+        to: { transform: "translateY(-100%)" },
+      },
+
+      animation: {
+        "slide-in": "slide 0.5s ease-in-out forwards",
+
+        "slide-back": "slideBack 0.5s ease-in-out delay-4000",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("tailwindcss-animation-delay")],
+};
