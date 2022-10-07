b8de3dba7fbc891cbc8c2340e5673d8bf4493e1e
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-10-07 16:26:21 +0200

    feat: complete error handling

--- src/subComponents/Error.tsx
+++ src/subComponents/Error.tsx
@@ -1,6 +1,8 @@
 import React, { useContext } from "react";
 import MyContext from "../context/DataContext";
 import { GrClose } from "react-icons/gr";
+import "../styles/animations.css"
+
 
 
 
@@ -22,16 +24,16 @@ const Error = () => {
     <>
       {hasError ? (  
        
-          <div className=" z-10 h-[20vh] absolute w-[100vw] animate-slide-in animate-slide-back  ">
-            <div className="flex items-center justify-between bg-yellow-500 text-white font-bold rounded-t px-4 py-2 h-[50%]">
-              <h1>Warning</h1>
+          <div id="slide" className=" z-10 h-[5vh] absolute w-[100vw]  sm:w-[20vw] animate-slide-in">
+            <div className="flex items-center justify-between bg-yellow-500 text-white font-bold  px-4 py-2 h-[50%]">
+              <h1 className=" md:text-xs">Warning</h1>
               <GrClose
                 className=" cursor-pointer hover:scale-150 ease-in-out duration-300 animate-pulse"
                 onClick={() => setHasError(false)}
               />
             </div>
             <div className="flex items-center border border-t-0 yellow-red-400 rounded-b bg-yellow-100 px-4 py-3 text-yellow-700 h-[50%]">
-                <p> Unfortunately your search was unsuccessful!!</p>
+                <p className=" md:text-xs  "> Search Error!!</p>
             </div>
           </div>
         
