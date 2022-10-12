63b97ee0d56a144fc0bfccc81502601736ee60f0
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-10-12 09:10:48 +0200

    feat: search header to appropriate color on scroll

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,47 @@
+import {  useEffect, useState } from "react";
+import {FaUnsplash} from "react-icons/fa";
 import SearchBar from "../subComponents/SearchBar";
 
 const Header = () => {
+  const [color, setColor] = useState("rgba(0, 0, 0, 0.3)");
+  const [textColor, setTextColor] = useState("white");
+ 
+  //const [navBarHeight, setNavBarHeight] = useState("90vh")
+  
+
+  useEffect(() => {
+    const changeColor = () => {
+      if (window.scrollY >= 675) {
+        setColor("white");
+        setTextColor("black");
+      
+        //setNavBarHeight("100px")
+      } else {
+        setColor("rgba(0, 0, 0, 0.3)");
+        setTextColor("white");
+       
+       // setNavBarHeight("150px")
+      }
+    };
+    window.addEventListener("scroll", changeColor);
+  }, []);
+
   return (
-    <div className=" bg-gray-900 flex items-center py-10 h-[200px]  ">
-      <div className="   w-full h-full flex flex-col items-center justify-between  ">
-        <h1 className=" text-white  text-2xl font-bold "> Find Images </h1>
-        <div>
+    <div
+      className=" fixed flex items-center py-10 w-full h-[10vh] duration-150 ease-in-out top-0 z-20 shadow-md"
+      style={{ backgroundColor: `${color}`}}
+    >
+      <div className="  gap-5 pl-5 flex items-center justify-center  ">
+        <h1
+          style={{ color: `${textColor}` }}
+          className=" text-white font-bold "
+        >
+         
+       <FaUnsplash className=" h-14 w-14"/>
+        </h1>
+       
           <SearchBar />
-        </div>
+       
       </div>
     </div>
   );
--- src/subComponents/SearchBar.tsx
+++ src/subComponents/SearchBar.tsx
@@ -11,16 +11,16 @@ const SearchBar = () => {
 >>>>>>> 4db4d7f (refactor: remove comments and clg from finished code)
 
   return (
-    <div className=" flex items-center border-2 rounded-lg py-2 shadow-sm w-[60vw] bg-white   ">
+    <div className=" flex items-center border-2 rounded-full py-2 shadow-sm w-[50vw] bg-white   ">
       <input
-        className=" pl-5 bg-transparent outline-none flex-grow text-lg text-black placeholder-gray-400  "
+        className=" pl-5 bg-transparent outline-none flex-grow text-sm text-black placeholder-gray-400  "
         type="text"
         placeholder="Search photos"
         value={inputValue}
         onChange={(e) => setInputValue(e.target.value)}
       />
       <AiOutlineSearch
-        className="h-12 w-12 bg-white text-gray-900 rounded-full p-2 cursor-pointer mx-2"
+        className="h-8 w-8 bg-gray-300 hover:bg-gray-400 hover:scale-105 duration-75 ease-in-out text-gray-900 rounded-full p-1 cursor-pointer mx-2"
         type="submit"
         onClick={() => fetchUnsplashImages(true)}
       />
