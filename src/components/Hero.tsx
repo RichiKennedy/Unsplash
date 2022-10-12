fb8fef8985c2adfba16154a51c2ffeb78527d7ae
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-10-12 09:12:33 +0200

    feat: add random unsplash image to header background

---
+++ src/components/Hero.tsx
@@ -0,0 +1,41 @@
+import React, { useContext, useEffect, useState } from "react";
+import MyContext from "../context/DataContext";
+
+const Hero = () => {
+  const { randomImage } = useContext(MyContext);
+  const [offSetY, setOffSetY] = useState(0);
+  const handleScroll = () => setOffSetY(window.pageYOffset);
+
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll)
+    return () => window.removeEventListener('scroll', handleScroll)
+  }, [])
+  // console.log(randomImage[0]?.urls?.small)
+
+  return (
+    <div 
+    
+    className="relative  h-[800px] bg-fixed z-10  ">
+      <img
+        height={600}
+        width={1000}
+        className=" object-cover w-full h-full"
+        src={randomImage[0]?.urls?.full}
+        alt={randomImage[0]?.alt_description ? randomImage[0].alt_description : ""}
+        key={randomImage[0]?.id}
+      />
+      <div className="absolute top-0 right-0 bottom-0 left-0 bg-black/20 text-white flex justify-between items-end">
+        <div className="">
+          <h1> Unsplash </h1>
+          <p>The internet’s source for visuals.
+Powered by creators everywhere.</p>
+        </div>
+<div className="flex items-center p-5 gap-2 h-[80px]">
+<h1 className=" text-gray-200"><strong>Photo</strong> by <strong> {randomImage[0]?.user.name} </strong> </h1>
+</div>
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
--- src/context/DataContext.tsx
+++ src/context/DataContext.tsx
@@ -1,5 +1,20 @@
 import { createContext, useState, ReactNode, useEffect } from "react";
 
+interface Urls {
+  full: string
+}
+
+interface User {
+  name: string
+}
+
+interface Image {
+  urls: Urls
+  alt_description: null | string
+  id: number
+  user: User
+}
+
 export type ContextShape = {
   fetchUnsplashImages: (arg?: boolean) => void;
   inputValue: string;
@@ -9,6 +24,9 @@ export type ContextShape = {
   setHasError: (arg: boolean) => void;
   hasApiError: boolean;
   setHasApiError: (arg: boolean) => void;
+  randomImage: Image[];
+  getRandomImage: () => void;
+ 
 };
 
 export type ContextProps = {
@@ -20,11 +38,13 @@ const REACT_APP_KEY = process.env.REACT_APP_KEY;
 
 export const MyContextProvider = ({ children }: ContextProps) => {
   const [images, setImages] = useState<Array<string>>([]);
+  const [randomImage, setRandomImage] = useState<Array<Image>>([]);
   const [page, setPage] = useState(1);
   const [inputValue, setInputValue] = useState("");
   const [hasError, setHasError] = useState(false);
   const [hasApiError, setHasApiError] = useState(false);
 
+ 
   useEffect(() => {
     if (hasError === true) {
       setTimeout(() => {
@@ -52,7 +72,12 @@ export const MyContextProvider = ({ children }: ContextProps) => {
         if (response.ok) {
           const jsonData = await response.json();
           const result = await jsonData.results;
+<<<<<<< HEAD
 
+=======
+         
+          // if your array is not empty, then display images,
+>>>>>>> 2540b2d (feat: add random unsplash image to header background)
           if (result.length > 1) {
             setImages(boolean ? [...result] : [...images, ...result]);
             setPage(page + 1);
@@ -71,13 +96,57 @@ export const MyContextProvider = ({ children }: ContextProps) => {
         setHasApiError(true);
       });
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 
     // const randomImage = async () => {
     //   fetch(`https://api.unsplash.com/photos/random?`)
     // }
 >>>>>>> 4db4d7f (refactor: remove comments and clg from finished code)
+=======
+>>>>>>> 2540b2d (feat: add random unsplash image to header background)
   };
+  
+  
+
+  const getRandomImage = () => {
+   
+    const REACT_APP_RANDOM_IMAGE_KEY = process.env.REACT_APP_RANDOM_IMAGE_KEY
+    const randomImageUrl = `https://api.unsplash.com/photos/random?count=1&orientation=landscape&client_id=${REACT_APP_RANDOM_IMAGE_KEY}`;
+
+    fetch(randomImageUrl).then(async (res) => {
+  
+      if (res.ok) {
+        const data = await res.json();
+        const result = await data;
+        if (result.length) {
+          setRandomImage(result);
+          console.log(result)
+        } else {
+          console.log('error: image does not exist')
+        }
+          
+      } if (!res.ok) {
+        console.log("Api error");
+      }
+    })
+    .catch((error) => {
+      console.log('Catch error')
+    })
+
+  }
+
+  useEffect(() => {
+    getRandomImage()
+  console.log('i fire once')
+  }, [])
+  // useEffect(() => {
+  //   if (!randomImage.length) {
+      
+
+  //     console.log('anything')
+  //   }
+  // }, []);
 
   return (
     <MyContext.Provider
@@ -90,6 +159,9 @@ export const MyContextProvider = ({ children }: ContextProps) => {
         setHasError,
         hasApiError,
         setHasApiError,
+        randomImage,
+        getRandomImage,
+        
       }}
     >
       {children}
