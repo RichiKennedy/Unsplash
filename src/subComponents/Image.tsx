7ba555bc1b32449f422a2d09a9569c0ae4fa6caa
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-10-25 15:17:50 +0200

    refactor: clean up code

--- src/subComponents/Image.tsx
+++ src/subComponents/Image.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "react-lazy-load-image-component/src/effects/blur.css";
 import { BsFillArrowDownSquareFill } from "react-icons/bs";
 import { ImageType } from "../types/imageTypes";
@@ -7,14 +8,15 @@ interface ImageProps {
 }
 
 const Image = ({ image, onImageClick }: ImageProps) => {
+
   const handleClick = (clickedImage: ImageType) => {
     onImageClick(clickedImage);
   };
 
   return (
-    <div className="elative w-[100%]">
+    <div className="  relative w-[100%]">
       <img
-        className="w-full object-cover h-full"
+        className="   w-full object-cover h-full   "
         key={image.id}
         src={image.urls.small}
         alt={image.alt_description}
@@ -25,7 +27,7 @@ const Image = ({ image, onImageClick }: ImageProps) => {
       >
         <div className="flex items-center p-5 gap-2 h-[80px]">
           <img
-            className="rounded-full h-10 w-10 "
+            className=" rounded-full h-10 w-10"
             src={image.user.profile_image.large}
             alt={image.alt_description}
           />
--- src/subComponents/Modal.tsx
+++ src/subComponents/Modal.tsx
@@ -12,7 +12,6 @@ const Modal = ({ modalImage }: ModalProps) => {
 
   const toggleZoom = () => {
     setZoomedIn(!zoomedIn);
-    console.log(zoomedIn)
   }
 
   useEffect(() => {
@@ -31,12 +30,12 @@ const Modal = ({ modalImage }: ModalProps) => {
     const classes = zoomedIn ? 'flex items-center justify-center max-h-[90%] min-w-[100%]' : 'w-[100%] h-[65%] sm:h-[80%] md:w-[90%] md:h-[80%] lg:w-[900px] lg:h-[900px] flex items-center justify-center'
     const src = zoomedIn ? modalImage.urls.raw : modalImage.urls.small
     const zoomCursor = zoomedIn ? 'hover: cursor-zoom-out opacity-0 hover:opacity-100 absolute top-0 right-0 bottom-0 left-0 flex justify-end' : 'hover: cursor-zoom-in opacity-0 hover:opacity-100 absolute top-0 right-0 bottom-0 left-0 flex justify-end'
-    const zoomIcon = zoomedIn ? <BsArrowsAngleContract className=" h-7 w-7 m-3 text-white" /> : <BsArrowsAngleExpand className=" h-7 w-7 m-3 text-white" />
+    const zoomIcon = zoomedIn ? <BsArrowsAngleContract className="h-7 w-7 m-3 text-white" /> : <BsArrowsAngleExpand className="h-7 w-7 m-3 text-white" />
     return (
       <section className={classes}>
-      <div className=" relative w-full h-full flex items-center justify-center ">
+      <div className="relative w-full h-full flex items-center justify-center ">
         <img
-          className="  object-cover w-full h-full"
+          className=" object-cover w-full h-full"
           src={src}
           alt={modalImage.alt_description}
         />
@@ -56,7 +55,6 @@ const Modal = ({ modalImage }: ModalProps) => {
         <div className="fixed top-0 left-0 z-50 w-screen h-[100vh] bg-black/70 flex flex-col justify-center items-center">
           <div className=" w-full">
             <MdOutlineClose
-              color="white"
               className="w-8 h-8 cursor-pointer fill-current lg:translate-y-10  text-white m-5 hover:scale-125 duration-150 ease-out -translate-y-10"
               onClick={() => closeModal()}
             />
@@ -65,7 +63,7 @@ const Modal = ({ modalImage }: ModalProps) => {
             <nav className="flex items-center justify-between  w-full ">
               <div className="flex items-center p-5 gap-2 h-[80px]">
                 <img
-                  className=" rounded-full h-10 w-10 "
+                  className="rounded-full h-10 w-10 "
                   src={modalImage.user.profile_image.large}
                   alt={modalImage.alt_description}
                 />
@@ -74,7 +72,6 @@ const Modal = ({ modalImage }: ModalProps) => {
                   target="_blank"
                   rel="noreferrer"
                 >
-                  
                   <h1 className=" hover:underline">
                     {modalImage.user.name}
                   </h1>
@@ -83,24 +80,24 @@ const Modal = ({ modalImage }: ModalProps) => {
               <a target="_blank" rel="noreferrer" href={`${modalImage.links.download_location}}&client_id=${REACT_APP_KEY}`} download>
               <button className="flex items-center justify-between gap-3 border px-2 py-1 rounded-md text-lg border-gray-400 hover:border-gray-900 text-gray-600 hover:text-gray-900 duration-150 ease-in-out m-5">
                 Download
-                <BsFillArrowDownSquareFill className=" w-5 h-5 rounded-md cursor-pointer " />
+                <BsFillArrowDownSquareFill className="w-5 h-5 rounded-md cursor-pointer" />
               </button>
               </a>
             </nav>
             {renderContent()}
-            <footer className=" w-full flex items-center justify-between ">
+            <footer className="w-full flex items-center justify-between">
               <ul className="flex items-center justify-between w-[70%] sm:w-[60%] md:w-[50%] lg:w-[35%] p-5">
-                <li className=" ">
+                <li>
                   <p className=" text-gray-400"> Views </p>
-                  <h1> {modalImage.views}</h1>
+                  <h1> {modalImage.views} </h1>
                 </li>
-                <li className=" ">
-                  <p className=" text-gray-400"> Downloads </p>
-                  <h1> {modalImage.downloads}</h1>
+                <li>
+                  <p className="text-gray-400"> Downloads </p>
+                  <h1> {modalImage.downloads} </h1>
                 </li>
-                <li className=" ">
-                  <p className=" text-gray-400"> Likes </p>
-                  <h1> {modalImage.likes}</h1>
+                <li>
+                  <p className="text-gray-400"> Likes </p>
+                  <h1> {modalImage.likes} </h1>
                 </li>
               </ul>
             </footer>
