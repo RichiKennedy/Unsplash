0c0d719fec869c1c3f2d56b37340719260365090
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-01 13:04:05 +0100

    feat: create route for about page

---
+++ src/pages/About.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import AboutHeader from "../components/AboutHeader";
+
+const About = () => {
+  return (
+    <div className="flex items-center justify-center flex-col w-[100vw]">
+      <AboutHeader />
+      <h1> About Page</h1>
+    </div>
+  );
+};
+
+export default About;
