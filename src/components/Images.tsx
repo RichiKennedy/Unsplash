a3e585667a5a13345bf330233fcd088a0d3a7a6a
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-01 09:05:36 +0100

    feat: finish Mosaic/Masonry image gallery

--- src/components/Images.tsx
+++ src/components/Images.tsx
@@ -1,33 +1,44 @@
 import InfiniteScroll from "react-infinite-scroll-component";
 import Loader from "../subComponents/Loader";
-import { useContext } from "react";
+import React, { useContext } from "react";
 import MyContext from "../context/DataContext";
 import Image from "../subComponents/Image";
 import { ImageType } from "../types/imageTypes";
+import Masonry from "react-smart-masonry";
 
 interface ImagesProps {
   onImageClick: (clickedImage: ImageType) => void;
- }
+}
 
-const Images = ({onImageClick}: ImagesProps) => {
+const Images = ({ onImageClick }: ImagesProps) => {
   const { images, hasInputValue, getRandomImages, fetchUnsplashImages } = useContext(MyContext);
- 
- return (
+  const breakpoints = { mobile: 0, tablet: 600, laptop: 1000, desktop: 1600 };
+  return (
     <>
-      <section className="flex items-center justify-center p-5  w-[100vw]">
+      <section className="flex items-center justify-center pt-10 w-[100vw]">
         <InfiniteScroll
           dataLength={images.length}
-          next={() =>
-            !hasInputValue ? getRandomImages() : fetchUnsplashImages()
-          }
+          next={ () => !hasInputValue ? getRandomImages() : fetchUnsplashImages() }
           hasMore={true}
           loader={<Loader />}
           hasChildren={true}
-          className="grid grid-cols-1 w-[100vw]  sm:grid-cols-2 md:p-5 lg:grid-cols-3 xl:p-20 gap-5"
+          className="max-w-[1200px]"
         >
-          {images.map((image: any) => (
-            <Image  image={image} key={image.id} onImageClick={(clickedImage: ImageType) => onImageClick(clickedImage)}/>
-          ))}
+          <Masonry
+            breakpoints={breakpoints}
+            columns={{ mobile: 1, tablet: 2, laptop: 3, desktop: 3 }}
+            gap={{ mobile: 20, tablet: 30, laptop: 30, desktop: 40 }}
+          >
+            {images.map((image: any) => (
+              <Image
+                image={image}
+                key={image.id}
+                onImageClick={(clickedImage: ImageType) =>
+                  onImageClick(clickedImage)
+                }
+              />
+            ))}
+          </Masonry>
         </InfiniteScroll>
       </section>
     </>
