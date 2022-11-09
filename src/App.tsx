95879503237a06c0ae9aea54af41d38448f14f92
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-09 13:38:08 +0100

    feat: add error and dynamic  route

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
 import { Routes, Route } from "react-router-dom";
+import { MyContextProvider } from "./context/DataContext";
 import About from "./pages/About";
 import Home from "./pages/Home";
+import NoPage from "./pages/NoPage";
+import Category from "./pages/Category";
 
 function App() {
   return (
+    <MyContextProvider>
     <Routes>
       <Route path="/" element={<Home/>} />
       <Route path="/about" element={<About/>} />
+      <Route path="*" element={<NoPage />} />
+      <Route path="/:categoryID" element={<Category />} />
     </Routes>
+    </MyContextProvider>
   );
 };
 
