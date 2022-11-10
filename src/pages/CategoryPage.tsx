62b235d802fcd3804866e203ad28e162723e0d83
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-10 16:27:03 +0100

    refactor: improve and move func to category-page

---
+++ src/pages/CategoryPage.tsx
@@ -0,0 +1,38 @@
+import React, { useContext, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import Header from '../components/Header';
+import Hero from '../components/Hero';
+import Images from '../components/Images';
+import MyContext from '../context/DataContext';
+import Modal from '../subComponents/Modal';
+import { ImageType } from '../types/imageTypes';
+
+interface CategoryPageProps {
+  onImageClick: (clickedImage: ImageType) => void;
+  modalImage: ImageType | undefined;
+  setModalImage: (arg: ImageType | undefined) => void
+}
+
+const CategoryPage = ({onImageClick, modalImage, setModalImage}: CategoryPageProps) => {
+  const { setTopic, setInputValue, fetchUnsplashImages, getSplashImage } = useContext(MyContext);
+  const {categoryID} = useParams()
+
+useEffect(() => {
+    setInputValue(categoryID || "")
+    setTopic(categoryID || "")
+
+    fetchUnsplashImages(true, 1, categoryID);
+    getSplashImage(categoryID)
+},[categoryID])
+
+  return (
+    <div>
+        <Header homePage={true} />
+        <Hero categoryID={categoryID}/>
+        {modalImage && <Modal modalImage={modalImage} setModalImage={setModalImage} />}
+        <Images onImageClick={onImageClick} />
+      </div>
+  )
+}
+
+export default CategoryPage
--- src/subComponents/CategoryItem.tsx
+++ src/subComponents/CategoryItem.tsx
@@ -1,35 +1,20 @@
-import React, { useContext, useEffect, useState } from 'react'
+import React from 'react'
 import { Link } from 'react-router-dom';
-import MyContext from '../context/DataContext';
 import { CategoryType } from '../types/categoryTypes';
 
 interface CategoryProps {
-    category: CategoryType
-    
+    category: CategoryType,
 }
 
 const CategoryItem = ({category}: CategoryProps) => {
-    const { hasInputValue, setHasInputValue, inputValue, setInputValue, fetchUnsplashImages } = useContext(MyContext);
-
-
-    const categorySearch = () => {
-   console.log('category is ' + category.name)
-   setInputValue(category.name)
-   fetchUnsplashImages(true)
-   console.log('category search()')
-    }
-  
-
   return (
-    <div>
-          <Link to={`/${category.id}`} >
-          <li 
-          onClick={() => categorySearch() }
-          className="cursor-pointer hover:underline underline-offset-8"> {category.name}
-           </li>
-          </Link>
-         
-    </div>
+    <Link 
+    className='cursor-pointer hover:underline underline-offset-4'
+    to={`/${category.id}`}>
+      
+        {category.name}
+      
+    </Link>
   )
 }
 
