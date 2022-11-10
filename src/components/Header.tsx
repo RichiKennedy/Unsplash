4a44967e827e7bc4903d738ed0bcc47aa2adf71e
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-10 16:29:52 +0100

    refactor: state change

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, CSSProperties } from "react";
 import { FaUnsplash } from "react-icons/fa";
 import { Link } from "react-router-dom";
 import CategoryItem from "../subComponents/CategoryItem";
@@ -6,64 +6,60 @@ import SearchBar from "../subComponents/SearchBar";
 import { CategoryType } from "../types/categoryTypes";
 
 interface HeaderProps {
-  isStatic: boolean;
+  homePage: boolean;
 }
 
-const Header = ({ isStatic }: HeaderProps) => {
+const Header = ({ homePage}: HeaderProps) => {
+  const [cssStyles, setCSSStyles] = useState<CSSProperties>({ backgroundColor: 'rgba(0, 0, 0, 0.3)',
+  color: 'white'});
 
-  const [color, setColor] = useState("rgba(0, 0, 0, 0.3)");
-  const [textColor, setTextColor] = useState("white");
-  let categories = [
+  const categories = [
     { name: "Architecture & Interiors", id: "Architecture & Interiors" },
     { name: "Ocean", id: "Ocean" },
-    { name: "Current Events", id: "CurrentEvents" },
-    { name: "Food & Drink", id: "Food&Drink" },
-    { name: "Fashion & Beauty", id: "Fashion&Beauty" },
-    { name: "Travel", id: "Travel" },
-    { name: "Street Art", id: "StreetArt" },
-    { name: "Nature", id: "Nature" },
+    { name: "Current Events", id: "Current Events" },
+    { name: "Food & Drink", id: "Food & Drink" },
+    { name: "Fashion & Beauty", id: "Fashion & Beauty" },
+    { name: "Interior Design", id: "Interior Design" },
+    { name: "Street Art", id: "Street Art" },
+    { name: "Nature", id: "Nature" }
   ];
   useEffect(() => {
     const changeColor = () => {
       if (window.scrollY >= 145) {
-        setColor("white");
-        setTextColor("black");
+        setCSSStyles({
+          backgroundColor: 'white',
+          color: 'black'
+        })
       } else {
-        setColor("rgba(0, 0, 0, 0.3)");
-        setTextColor("white");
+        setCSSStyles({
+          backgroundColor: 'rgba(0, 0, 0, 0.3)',
+          color: 'white'
+        })
       }
     };
-    window.addEventListener("scroll", changeColor);
+    
+    if (!homePage) {
+      setCSSStyles({
+        backgroundColor: 'white',
+        color: 'black'
+      })
+    }
+    else {
+      window.addEventListener("scroll", changeColor);
+    }
   }, []);
 
-  const styleOnLocation = () => {
-    const navLink = isStatic ? (
-      <div
-        style={{ color: `${textColor}` }}
-        className="  flex items-center w-50 justify-center gap-5 pr-5"
-      >
-        
-          <h1> | </h1>
-        <Link to="/about">
-          <h1 className="cursor-pointer hover:underline underline-offset-8 ">
-            About
-          </h1>
-        </Link>
-      </div>
-    ) : null;
-    const containerStyle = isStatic ? `${color}` : "white";
-    const iconColor = isStatic ? `${textColor}` : "black";
     return (
       <main
         className="fixed flex justify-start w-[100vw] h-[12vh] lg:h-[18vh] duration-150 ease-in-out top-0 z-20 shadow-md"
-        style={{ backgroundColor: containerStyle }}
+        style={{ backgroundColor: cssStyles.backgroundColor, color: cssStyles.color  }}
+       
       >
-        <section className="flex items-center justify-between h-full w-full ">
-          <div className="  h-[100%] w-[100%] flex flex-col items-start justify-evenly px-5">
+        <section className="flex items-center justify-between h-full w-full">
+          <div className="h-[100%] w-[100%] flex flex-col items-start justify-evenly px-5">
             <div className="gap-5 flex w-full ">
               <Link to="/">
-                <FaUnsplash
-                  style={{ color: iconColor }}
+                <FaUnsplash    
                   className="h-11 w-11 sm:h-14 sm:w-14 cursor-pointer"
                 />
               </Link>
@@ -71,41 +67,26 @@ const Header = ({ isStatic }: HeaderProps) => {
             </div>
             <div className="flex  w-full">
             <ul
-              style={{ color: `${textColor}` }}
-              className="w-[1600px] flex items-center justify-between overflow-auto whitespace-nowrap gap-5 "
+              className="w-[1600px] flex items-center justify-between overflow-auto whitespace-nowrap gap-5"
             >
               {categories.map((category: CategoryType) => (
                 <CategoryItem category={category} />
               ))}
-              
             </ul>
-              <ul className="flex items-center justify-end gap-5 w-[20%]"> {navLink} </ul>
-
+            
+            <ul 
+             className="flex items-center justify-end gap-5 w-[20%]">
+              <CategoryItem category={{
+                id:'about',
+                name: 'About'
+              }} />
+            </ul>
+            
             </div>
           </div>
-          
         </section>
       </main>
     );
   };
-
-  return (
-    // <main
-    //   className="fixed flex items-center py-10 w-full h-[10vh] duration-150 ease-in-out top-0 z-20 shadow-md"
-    //   style={{ backgroundColor: `${color}` }}
-    // >
-    //   <div className="gap-5 pl-5 flex items-center justify-center">
-    //     <Link to="/">
-    //       <FaUnsplash
-    //         style={{ color: `${textColor}` }}
-    //         className="h-11 w-11 sm:h-14 sm:w-14 cursor-pointer"
-    //       />
-    //     </Link>
-    //     <SearchBar />
-    //   </div>
-
-    styleOnLocation()
-  );
-};
 
 export default Header;
