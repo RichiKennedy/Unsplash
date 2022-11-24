2045267a0ea8e7708479494535a078b03b439cb7
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-24 17:01:32 +0100

    test: no page component renders

---
+++ src/pages/404ErrorPage/NoPage.test.tsx
@@ -0,0 +1,13 @@
+import { render, screen } from "@testing-library/react"
+import { BrowserRouter } from "react-router-dom"
+import NoPage from "./NoPage"
+
+it('should render no page component', () => {
+    render(
+        <BrowserRouter>
+        <NoPage />
+        </BrowserRouter>
+    )
+    const noPage = screen.getByTestId('no-page-wrapper');
+    expect(noPage).toBeDefined();
+})
