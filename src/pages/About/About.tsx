b1543aa5d9b0907321c3e63ccaffdd069bd609fc
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-29 10:33:36 +0100

    fix: linting bugs in about page

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,19 +1,24 @@
-import React from "react";
-import Header from "../../components/Header/Header";
+import React from 'react'
+import Header from '../../components/Header/Header'
 
-const About = () => {
-  return (
-    <section 
+const About = () => (
+  <section
     data-test="about-wrapper"
-    className="flex items-center justify-center flex-col bg-gray-800 w-[100vw] h-[100vh] ">
-      <Header homePage={false}/>
-      <div className=" flex items-center justify-center p-10 w-[50%] flex-col text-white">
-        <h1 className=" text-4xl"> About Page</h1>
-        <p> Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque corrupti voluptatum enim recusandae iusto voluptatibus aliquam laborum ea cumque asperiores quae labore, laboriosam libero perspiciatis. Reiciendis voluptates cupiditate, repellat aperiam quam saepe numquam, alias dolore minus aut tenetur praesentium sapiente hic quis possimus autem laborum unde natus quidem quibusdam provident?
-        </p>
-      </div>
-    </section>
-  );
-};
+    className="flex items-center justify-center flex-col bg-gray-800 w-[100vw] h-[100vh] "
+  >
+    <Header homePage={false} />
+    <div className=" flex items-center justify-center p-10 w-[50%] flex-col text-white">
+      <h1 className=" text-4xl"> About Page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque corrupti
+        voluptatum enim recusandae iusto voluptatibus aliquam laborum ea cumque
+        asperiores quae labore, laboriosam libero perspiciatis. Reiciendis
+        voluptates cupiditate, repellat aperiam quam saepe numquam, alias dolore
+        minus aut tenetur praesentium sapiente hic quis possimus autem laborum
+        unde natus quidem quibusdam provident?
+      </p>
+    </div>
+  </section>
+)
 
-export default About;
+export default About
