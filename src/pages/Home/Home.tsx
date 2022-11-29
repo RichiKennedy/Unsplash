7062389d3afc69db084a94d7288bf916f5ab1a08
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-29 10:35:09 +0100

    fix: linting bugs in home page

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,26 +1,38 @@
-import React from "react";
-import Header from "../../components/Header/Header";
-import Hero from "../../components/hero/Hero";
-import Modal from "../../subComponents/Modal";
-import { ImageType } from "../../types/imageTypes"
-import ImageGallery from "../../components/ImageGallery/ImageGallery";
+import React from 'react'
+import Header from '../../components/Header/Header'
+import Hero from '../../components/hero/Hero'
+import Modal from '../../subComponents/Modal'
+import { ImageType } from '../../types/imageTypes'
+import ImageGallery from '../../components/ImageGallery/ImageGallery'
+
 interface HomeProps {
-  onImageClick: (clickedImage: ImageType) => void;
-  modalImage: ImageType | undefined;
+  onImageClick: (clickedImage: ImageType) => void
+  modalImage: ImageType | undefined
   setModalImage: (arg: ImageType | undefined) => void
-  isOpen: boolean;
-  setIsOpen: (arg: boolean) => void;
+  isOpen: boolean
+  setIsOpen: (arg: boolean) => void
 }
 
-const Home = ({onImageClick, modalImage, setModalImage, isOpen, setIsOpen}: HomeProps) => {
-  return (
-    <div data-test='home-wrapper' >
-      <Header homePage={true}/>
-      <Hero categoryID={undefined}  />
-      {modalImage && <Modal modalImage={modalImage} setModalImage={setModalImage} isOpen={isOpen} setIsOpen={setIsOpen} />}
-      <ImageGallery onImageClick={onImageClick} />
-    </div>
-  );
-};
+const Home = ({
+  onImageClick,
+  modalImage,
+  setModalImage,
+  isOpen,
+  setIsOpen,
+}: HomeProps) => (
+  <div data-test="home-wrapper">
+    <Header homePage />
+    <Hero categoryID={undefined} />
+    {modalImage && (
+      <Modal
+        modalImage={modalImage}
+        setModalImage={setModalImage}
+        isOpen={isOpen}
+        setIsOpen={setIsOpen}
+      />
+    )}
+    <ImageGallery onImageClick={onImageClick} />
+  </div>
+)
 
-export default Home;
+export default Home
