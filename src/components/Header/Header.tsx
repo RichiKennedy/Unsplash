146c3d497f0b4c2b514056460d7daa4e49952d8d
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-29 10:30:19 +0100

    fix: linting bugs in header component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,56 +1,56 @@
-import { useEffect, useState, CSSProperties } from "react";
-import { FaUnsplash, FaAngleRight } from "react-icons/fa";
-import { Link } from "react-router-dom";
-import ApiError from "../../subComponents/ApiError";
-import CategoryItem from "../../subComponents/CategoryItem";
-import Error from "../../subComponents/Error";
-import SearchBar from "../../subComponents/SearchBar";
-import { CategoryType } from "../../types/categoryTypes";
+import React, { useEffect, useState, CSSProperties } from 'react'
+import { FaUnsplash, FaAngleRight } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+import ApiError from '../../subComponents/ApiError'
+import CategoryItem from '../../subComponents/CategoryItem'
+import Error from '../../subComponents/Error'
+import SearchBar from '../../subComponents/SearchBar'
+import { CategoryType } from '../../types/categoryTypes'
 
 interface HeaderProps {
-  homePage: boolean;
+  homePage: boolean
 }
 
 const Header = ({ homePage }: HeaderProps) => {
   const [cssStyles, setCSSStyles] = useState<CSSProperties>({
-    backgroundColor: "rgba(0, 0, 0, 0.3)",
-    color: "white",
-  });
+    backgroundColor: 'rgba(0, 0, 0, 0.3)',
+    color: 'white',
+  })
 
   const categories = [
-    { name: "Architecture & Interiors", id: "Architecture & Interiors" },
-    { name: "Ocean", id: "Ocean" },
-    { name: "Current Events", id: "Current Events" },
-    { name: "Food & Drink", id: "Food & Drink" },
-    { name: "Fashion & Beauty", id: "Fashion & Beauty" },
-    { name: "Interior Design", id: "Interior Design" },
-    { name: "Street Art", id: "Street Art" },
-    { name: "Nature", id: "Nature" },
-  ];
+    { name: 'Architecture & Interiors', id: 'Architecture & Interiors' },
+    { name: 'Ocean', id: 'Ocean' },
+    { name: 'Current Events', id: 'Current Events' },
+    { name: 'Food & Drink', id: 'Food & Drink' },
+    { name: 'Fashion & Beauty', id: 'Fashion & Beauty' },
+    { name: 'Interior Design', id: 'Interior Design' },
+    { name: 'Street Art', id: 'Street Art' },
+    { name: 'Nature', id: 'Nature' },
+  ]
   useEffect(() => {
     const changeColor = () => {
       if (window.scrollY >= 100) {
         setCSSStyles({
-          backgroundColor: "white",
-          color: "black",
-        });
+          backgroundColor: 'white',
+          color: 'black',
+        })
       } else {
         setCSSStyles({
-          backgroundColor: "rgba(0, 0, 0, 0.3)",
-          color: "white",
-        });
+          backgroundColor: 'rgba(0, 0, 0, 0.3)',
+          color: 'white',
+        })
       }
-    };
+    }
 
     if (!homePage) {
       setCSSStyles({
-        backgroundColor: "white",
-        color: "black",
-      });
+        backgroundColor: 'white',
+        color: 'black',
+      })
     } else {
-      window.addEventListener("scroll", changeColor);
+      window.addEventListener('scroll', changeColor)
     }
-  }, []);
+  }, [])
 
   return (
     <main
@@ -61,7 +61,7 @@ const Header = ({ homePage }: HeaderProps) => {
         color: cssStyles.color,
       }}
     >
-         <Error />
+      <Error />
       <ApiError />
       <section className="flex items-center justify-between h-full w-full">
         <div className="h-[100%] w-[100%] flex flex-col items-start justify-evenly px-5">
@@ -73,16 +73,16 @@ const Header = ({ homePage }: HeaderProps) => {
           </div>
           <div className="flex w-full">
             <ul className="w-[1200px] xl:w-[90%] flex items-center justify-between overflow-auto whitespace-nowrap gap-5">
-              {categories.map((category: CategoryType, key: number) => (
-                <CategoryItem category={category} key={key} />
+              {categories.map((category: CategoryType) => (
+                <CategoryItem category={category} key={category.id} />
               ))}
             </ul>
             <ul className="flex items-center justify-between xl:justify-end px-5 gap-5 w-[400px] ">
               <FaAngleRight className="text-lg xl:hidden block" />
               <CategoryItem
                 category={{
-                  id: "about",
-                  name: "About",
+                  id: 'about',
+                  name: 'About',
                 }}
               />
             </ul>
@@ -90,7 +90,7 @@ const Header = ({ homePage }: HeaderProps) => {
         </div>
       </section>
     </main>
-  );
-};
+  )
+}
 
-export default Header;
+export default Header
