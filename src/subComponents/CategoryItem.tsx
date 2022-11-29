95a1047b41a0f26e4b81289befd716e5727b19f0
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-29 10:36:09 +0100

    fix: linting bugs in category item component

--- src/subComponents/CategoryItem.tsx
+++ src/subComponents/CategoryItem.tsx
@@ -1,20 +1,18 @@
-import React from "react";
-import { Link } from "react-router-dom";
-import { CategoryType } from "../types/categoryTypes";
+import React from 'react'
+import { Link } from 'react-router-dom'
+import { CategoryType } from '../types/categoryTypes'
 
 interface CategoryProps {
-  category: CategoryType;
+  category: CategoryType
 }
 
-const CategoryItem = ({ category }: CategoryProps) => {
-  return (
-    <Link
-      className="cursor-pointer hover:underline underline-offset-4"
-      to={`/${category.id}`}
-    >
-      {category.name}
-    </Link>
-  );
-};
+const CategoryItem = ({ category }: CategoryProps) => (
+  <Link
+    className="cursor-pointer hover:underline underline-offset-4"
+    to={`/${category.id}`}
+  >
+    {category.name}
+  </Link>
+)
 
-export default CategoryItem;
+export default CategoryItem
