92721b9bc64b6d84829d732984d2d5412a0d88d7
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-29 10:38:50 +0100

    fix: linting bugs in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,63 @@
-import { Routes, Route } from "react-router-dom";
-import { MyContextProvider } from "./context/DataContext";
-import About from "./pages/About/About";
-import Home from "./pages/Home/Home";
-import NoPage from "./pages/404ErrorPage/NoPage";
-import CategoryPage from "./pages/CategoryPage/CategoryPage";
-import { ImageType } from "./types/imageTypes";
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import { MyContextProvider } from './context/DataContext'
+import About from './pages/About/About'
+import Home from './pages/Home/Home'
+import NoPage from './pages/404ErrorPage/NoPage'
+import CategoryPage from './pages/CategoryPage/CategoryPage'
+import { ImageType } from './types/imageTypes'
 
 function App() {
-  const [isOpen, setIsOpen] = useState(false);
-  const [modalImage, setModalImage] = useState<ImageType | undefined> (undefined);
+  const [isOpen, setIsOpen] = useState(false)
+  const [modalImage, setModalImage] = useState<ImageType | undefined>(undefined)
   const [scrollPosition, setScrollPosition] = useState(0)
-  
+
   const onImageClick = (clickedImage: ImageType) => {
     setModalImage(clickedImage)
-  };
-  
+  }
+
   useEffect(() => {
     if (isOpen) {
       setScrollPosition(window.scrollY)
-      document.body.style.position = "fixed"
+      document.body.style.position = 'fixed'
     } else {
-      document.body.style.position = ""
+      document.body.style.position = ''
       window.scrollTo(0, scrollPosition)
     }
   }, [isOpen])
 
   return (
     <MyContextProvider>
-    <Routes>
-      <Route path="/" element={<Home modalImage={modalImage} setModalImage={setModalImage} onImageClick={onImageClick} isOpen={isOpen} setIsOpen={setIsOpen}/>} />
-      <Route path="/about" element={<About/>} />
-      <Route path="*" element={<NoPage />} />
-      <Route path="/:categoryID" element={<CategoryPage onImageClick={onImageClick} modalImage={modalImage} setModalImage={setModalImage} isOpen={isOpen} setIsOpen={setIsOpen} />} />
-    </Routes>
+      <Routes>
+        <Route
+          path="/"
+          element={
+            <Home
+              modalImage={modalImage}
+              setModalImage={setModalImage}
+              onImageClick={onImageClick}
+              isOpen={isOpen}
+              setIsOpen={setIsOpen}
+            />
+          }
+        />
+        <Route path="/about" element={<About />} />
+        <Route path="*" element={<NoPage />} />
+        <Route
+          path="/:categoryID"
+          element={
+            <CategoryPage
+              onImageClick={onImageClick}
+              modalImage={modalImage}
+              setModalImage={setModalImage}
+              isOpen={isOpen}
+              setIsOpen={setIsOpen}
+            />
+          }
+        />
+      </Routes>
     </MyContextProvider>
-  );
-};
+  )
+}
 
-export default App;
+export default App
