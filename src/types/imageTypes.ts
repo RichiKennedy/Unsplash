b3b606e34cba640f48f3290912465d6eb782d546
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-29 10:39:24 +0100

    fix: linting bugs for types pages

--- src/types/imageTypes.ts
+++ src/types/imageTypes.ts
@@ -1,40 +1,40 @@
 interface Urls {
-  full: string;
-  raw: string;
-  regular: string;
-  small: string;
+  full: string
+  raw: string
+  regular: string
+  small: string
 }
 interface ProfileImage {
-  large: string;
-  medium: string;
-  small: string;
+  large: string
+  medium: string
+  small: string
 }
 
 interface User {
-  name: null | string;
-  profile_image: ProfileImage;
-  links: Links;
-  for_hire: boolean;
+  name: null | string
+  profile_image: ProfileImage
+  links: Links
+  for_hire: boolean
 }
 interface Links {
-  html: string;
-  download_location: string;
+  html: string
+  download_location: string
 }
 
 interface Location {
-  city: string;
-  country: string;
+  city: string
+  country: string
 }
 
 export interface ImageType {
-  urls: Urls;
-  alt_description: string;
-  id: string;
-  user: User;
-  views: number;
-  downloads: number;
-  likes: number;
-  location: Location;
-  links: Links;
-  description: string;
+  urls: Urls
+  alt_description: string
+  id: string
+  user: User
+  views: number
+  downloads: number
+  likes: number
+  location: Location
+  links: Links
+  description: string
 }
