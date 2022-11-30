f84bbf3297b2acfc78f73a1985eb35601270bf5f
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-11-30 16:07:32 +0100

    refactor: add test id to element

--- src/subComponents/ErrorComponents/Error.tsx
+++ src/subComponents/ErrorComponents/Error.tsx
@@ -16,12 +16,14 @@ const Error = () => {
     <section>
       {hasError ? (
         <div
+          data-test="error-wrapper"
           id="slide"
           className=" z-50 h-[5vh] absolute w-[100vw]  sm:w-[20vw] animate-slide-in"
         >
           <div className="flex items-center justify-between bg-yellow-500 text-white font-bold  px-4 py-2 h-[50%]">
             <h1 className=" md:text-xs">Warning</h1>
             <GrClose
+              data-test="button"
               className=" cursor-pointer hover:scale-150 ease-in-out duration-300 animate-pulse"
               onClick={() => setHasError(false)}
             />
