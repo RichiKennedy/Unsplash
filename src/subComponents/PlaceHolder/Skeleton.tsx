a9b08043566c03d9c737e0cac5b5dc8718c799e3
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-12-12 10:44:27 +0100

    refactor: create folder for component and test file

--- src/subComponents/PlaceHolder/Skeleton.tsx
+++ src/subComponents/PlaceHolder/Skeleton.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 import { BsImage } from 'react-icons/bs'
 
 const Skeleton = () => (
-  <main className="flex items-center justify-center bg-neutral-900 w-full h-full absolute">
+  <main
+    data-test="skeleton-wrapper"
+    className="flex items-center justify-center bg-neutral-900 w-full h-full absolute"
+  >
     <section className="flex items-center justify-center gap-5 w-[100%] h-[100%]">
       <div className="relative w-[90%] h-[90%] space-y-3 overflow-hidden rounded-md bg-neutral-800 p-3 shadow before:absolute before:inset-0 before:-translate-x-full before:bg-gradient-to-r before:from-transparent before:via-white/20 hover:shadow-lg before:animate-[shimmer_1.5s_infinite]">
         <div className="h-[90%] w-[full  rounded-lg">
