577912a3585771042d04149657ef4cf91e922777
Richi Kennedy <90832346+RichiKennedy@users.noreply.github.com>
2022-12-14 10:51:19 +0100

    refactor: move categories array into export file

---
+++ src/pages/CategoryPage/CategoryData.ts
@@ -0,0 +1,11 @@
+const categories = [
+  { name: 'Architecture & Interiors', id: 'Architecture & Interiors' },
+  { name: 'Ocean', id: 'Ocean' },
+  { name: 'Current Events', id: 'Current Events' },
+  { name: 'Food & Drink', id: 'Food & Drink' },
+  { name: 'Fashion & Beauty', id: 'Fashion & Beauty' },
+  { name: 'Interior Design', id: 'Interior Design' },
+  { name: 'Street Art', id: 'Street Art' },
+  { name: 'Nature', id: 'Nature' },
+]
+export default categories
